23014f13e90a56faee2c886752864d3f22331e94
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-07-26 16:55:21 -0400

    Add RSpec & Update Javascript Config Generator

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module NeracEvac
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Rails.application.configure do
+      config.generators.javascript_engine = :js
+    end
   end
 end
